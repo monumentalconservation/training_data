f290aa1bfd4922140c3dc594ca9656b75c33970f
Rosie-Brigham <rosiebrigham@gmail.com>
2021-01-11 19:33:19 +0000

    adds validation images

--- save-images-no-dups.py
+++ save-images-no-dups.py
@@ -5,7 +5,7 @@ from PIL import ImageOps
 
 
 # This gets all the images from the file - saves them, and flips them
-with open('ims.json') as data_file:    
+with open('validation.json') as data_file:    
     data = json.load(data_file)
     i = 0
     for image in data:
@@ -17,15 +17,15 @@ with open('ims.json') as data_file:
             
             # Save the image
             imageURL = image['Labeled Data']
-            img = urllib.request.urlretrieve(imageURL, f"images/{i}.jpeg")
+            img = urllib.request.urlretrieve(imageURL, f"val_images/{i}.jpeg")
             # ensure no RGBA sneaks in...
-            img = Image.open(f"images/{i}.jpeg").convert('RGB').save(f"images/{i}.jpeg")
+            img = Image.open(f"val_images/{i}.jpeg").convert('RGB').save(f"val_images/{i}.jpeg")
 
 
             # # then save the label
             segmented = image['Label']['objects'][0]['instanceURI']
-            img = urllib.request.urlretrieve(segmented, f"masks/{i}.png")
-            img = Image.open(f"masks/{i}.png").convert('RGB')
-            img.save(f"masks/{i}.png")
+            img = urllib.request.urlretrieve(segmented, f"val_masks/{i}.png")
+            img = Image.open(f"val_masks/{i}.png").convert('RGB')
+            img.save(f"val_masks/{i}.png")
            
 
